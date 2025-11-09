65d7791c328461c1850f4f9aa6a10e4358d054cf
sfmorgan <s.morgan123@hotmail.com>
2025-11-09 14:55:26 +0000

    testing

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import pandas as pd
 from openpyxl import load_workbook
 from pathlib import Path
 
-data = {name: pd.read_excel(Path("data") / f"{name}.xlsx", sheet_name=None) for name in ["Armor", "Skill", "Talent", "Weapon"]}
+from platformdirs import user_data_dir
+
+root_directoy = Path(user_data_dir("Damage Calculator", "iRNHO"))
+
+data = {name: pd.read_excel(root_directoy / "data" / f"{name}.xlsx", sheet_name=None) for name in ["Armor", "Skill", "Talent", "Weapon"]}
 
 print(data.keys())
 print(data["Armor"]["Exotic"].head())
