bfa4492dc52508c11a77d1bde2d17f4bdcb5e44c
sfmorgan <s.morgan123@hotmail.com>
2025-11-09 14:48:47 +0000

    Testing stuff

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 from openpyxl import load_workbook
 from pathlib import Path
 
-data = {name: pd.read_excel(Path("data") / f"{name}.xlsx") for name in ["Armor", "Skill", "Talent", "Weapon"]}
+data = {name: pd.read_excel(Path("data") / f"{name}.xlsx", sheet_name=None) for name in ["Armor", "Skill", "Talent", "Weapon"]}
 
 print(data.keys())
 print(data["Armor"]["Exotic"].head())
